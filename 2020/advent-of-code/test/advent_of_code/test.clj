d1f09f4715087b3687539eeec6821f53972677e3
Stefan Arentz <stefan@arentz.ca>
2020-12-05 01:22:47 +0000

    Day 12, 2019 - tests

--- 2020/advent-of-code/test/advent_of_code/test.clj
+++ 2020/advent-of-code/test/advent_of_code/test.clj
@@ -1,9 +1,20 @@
 (ns advent-of-code.test
   (:require [clojure.test :refer :all]
+            [advent-of-code.2019.day12]
             [advent-of-code.day1 :as day1]
             [advent-of-code.day2 :as day2]
             [advent-of-code.day3 :as day3]
             [advent-of-code.day4 :as day4]))
+
+;; 2019
+
+(deftest year-2019-day-12-test
+  (testing "Year 2019, Day 12, Part 1"
+    (is (= 10635 (advent-of-code.2019.day12/part1))))
+  (testing "Year 2019, Day 12, Part 2"
+    (is (= 583523031727256 (advent-of-code.2019.day12/part2)))))
+
+;; 2020
 
 (deftest day1-test
   (testing "Day 1, Part 1"
