b9017a291459bdd8116d9224f163c01d96d55618
Stefan Arentz <stefan@arentz.ca>
2020-12-11 11:57:40 +0000

    Day 10, 2020

--- 2020/advent-of-code/src/advent_of_code/day10.clj
+++ 2020/advent-of-code/src/advent_of_code/day10.clj
@@ -13,30 +13,8 @@
 (defn matching-adapter [adapters rating]
   (first (filter #(<= % (+ rating 3)) (sort adapters))))
 
-(matching-adapter [16 10 15 5 1 11 7 19 6 12 4]  0)
-(matching-adapter [16 10 15 5   11 7 19 6 12 4]  1)
-(matching-adapter [16 10 15 5   11 7 19 6 12  ]  4)
-(matching-adapter [16 10 15     11 7 19 6 12  ]  5)
-(matching-adapter [16 10 15     11 7 19   12  ]  6)
-(matching-adapter [16 10 15     11   19   12  ]  7)
-(matching-adapter [16    15     11   19   12  ] 10)
-(matching-adapter [16    15          19   12  ] 11)
-(matching-adapter [16    15          19       ] 12)
-(matching-adapter [16                19       ] 15)
-(matching-adapter [                  19       ] 16) ;; 19
-
-
-
-(frequencies (map #(* -1 (apply - %)) (partition 2 1 [0 1 4 5 6 7 10 11 12 15 16 19 22])))
-;; => {1 7, 3 5}
-
 (defn jolt-distribution [jolts]
   (frequencies (map #(* -1 (apply - %)) (partition 2 1 jolts))))
-
-(jolt-distribution [0 1 4 5 6 7 10 11 12 15 16 19 22])
-;; => {1 6, 3 4, 0 1, 4 1}
-
-;;(matching-adapter
 
 (defn order-adapters [input]
   (loop [adapters (set input) result [0]]
