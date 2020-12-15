a9b0a5b4ebf47d49d4b86a1c24a19f8214f49e42
Stefan Arentz <stefan@arentz.ca>
2020-12-15 02:24:16 +0000

    Adding math.numeric-tower

--- 2020/advent-of-code/project.clj
+++ 2020/advent-of-code/project.clj
@@ -5,5 +5,6 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/math.combinatorics "0.1.6"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
                  [ubergraph "0.8.2"]]
   :repl-options {:init-ns advent-of-code.core})
