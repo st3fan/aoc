e4eb0b88d1ed00893b57540ab2962849e216f908
Stefan Arentz <stefan@arentz.ca>
2020-12-30 11:31:47 -0500

    Updates to util.clj

--- 2020/advent-of-code/src/advent_of_code/util.clj
+++ 2020/advent-of-code/src/advent_of_code/util.clj
@@ -10,9 +10,33 @@
 
 (defn load-input
   ([year day]
-   (load-input year day identity))
+   (load-input year day identity (complement nil?)))
   ([year day transformer]
-   (mapv transformer (line-seq (io/reader (io/resource (str "advent_of_code/" year "/day" day "/input")))))))
+   (load-input year day transformer (complement nil?)))
+  ([year day transformer filter-pred]
+   (->> (str "advent_of_code/" year "/day" day "/input")
+       io/resource
+       io/reader
+       line-seq
+       (filter filter-pred)
+       (mapv transformer))))
+
+(defn load-test-input
+  ([year day]
+   (load-test-input year day identity (complement nil?)))
+  ([year day transformer]
+   (load-test-input year day transformer (complement nil?)))
+  ([year day transformer filter-pred]
+   (->> (str "advent_of_code/" year "/day" day "/test")
+       io/resource
+       io/reader
+       line-seq
+       (filter filter-pred)
+       (mapv transformer))))
+
+;; (mapv transformer
+;;   (filter filter-pred
+;;     (doall (line-seq (io/reader (io/resource (str "advent_of_code/" year "/day" day "/input")))))))))
 
 (defn load-input-string
   ([year day]
