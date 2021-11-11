87cb6a6282c4af6aca62c82040f49462763513b5
Stefan Arentz <stefan@arentz.ca>
2021-11-11 10:12:59 -0500

    2017 Day 13

--- 2017/day13.py
+++ 2017/day13.py
@@ -66,6 +66,25 @@ if __name__ == "__main__":
 
     # Part 2
 
+    #
+    # Now, you need to pass through the firewall without being caught - easier
+    # said than done.
+    #
+    # You can't control the speed of the packet, but you can delay it any
+    # number of picoseconds. For each picosecond you delay the packet before
+    # beginning your trip, all security scanners move one step. You're not in
+    # the firewall during this time; you don't enter layer 0 until you stop
+    # delaying the packet.
+    #
+    # If there is a scanner at the top of the layer as your packet enters it,
+    # you are caught. (If a scanner moves into the top of its layer while you
+    # are there, you are not caught: it doesn't have time to notice you before
+    # you leave.)
+    #
+    # What is the fewest number of picoseconds that you need to delay the
+    # packet to pass through the firewall without being caught?
+    #
+
     layers = {}
     with open("day13.input") as fp:
         for line in [line.strip() for line in fp.readlines()]:
@@ -74,7 +93,7 @@ if __name__ == "__main__":
 
     for delay in range(100000):
 
-        for _ in range(delay):
+        for _ in range(10, delay):
             for layer in layers.values():
                 layer.tick()
 
