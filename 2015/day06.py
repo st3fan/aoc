fc4ddac923c0918ffe115c4d8444590777f2faa3
Stefan Arentz <stefan@arentz.ca>
2021-11-12 09:06:34 -0500

    2015 Day 6

---
+++ 2015/day06.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+
+from aoc import Position
+from dataclasses import dataclass
+
+
+@dataclass
+class Instruction:
+    fr: Position
+    to: Position
+    op: str
+
+    @classmethod
+    def from_string(cls, s):
+        c = s.split()
+        if s.startswith("toggle"):
+            return cls(Position.from_string(c[-3]), Position.from_string(c[-1]), "toggle")
+        else:
+            return cls(Position.from_string(c[-3]), Position.from_string(c[-1]), c[1])
+
+
+class Grid:
+    def __init__(self, width, height, default=None):
+        self.width = width
+        self.height = height
+        self.nodes = [default] * (width * height)
+
+    def get(self, p):
+        return self.nodes[p.x + (p.y * self.width)]
+
+    def set(self, p, v, default=None):
+        self.nodes[p.x + (p.y * self.width)] = v
+
+    def count(self, value):
+        return sum(node == value for node in self.nodes)
+
+
+
+def main():
+    
+    instructions = [Instruction.from_string(line.strip()) for line in open("day06.input").readlines()]
+
+    # Part 1
+
+    grid = Grid(1000, 1000, False)
+
+    for i in instructions:
+        for y in range(i.fr.y, i.to.y+1):
+            for x in range(i.fr.x, i.to.x+1):
+                match i.op:
+                    case "toggle":
+                        grid.set(Position(x, y), not grid.get(Position(x, y)))
+                    case "on":
+                        grid.set(Position(x, y), True)
+                    case "off":
+                        grid.set(Position(x, y), False)
+
+    print("Part one:", grid.count(True))
+
+    # Part 2
+
+    grid = Grid(1000, 1000, 0)
+
+    for i in instructions:
+        for y in range(i.fr.y, i.to.y+1):
+            for x in range(i.fr.x, i.to.x+1):
+                match i.op:
+                    case "toggle":
+                        grid.set(Position(x, y), grid.get(Position(x, y)) + 2)
+                    case "on":
+                        grid.set(Position(x, y), grid.get(Position(x, y)) + 1)
+                    case "off":
+                        grid.set(Position(x, y), max(0, grid.get(Position(x, y)) - 1))
+
+    print("Part two:", sum(node for node in grid.nodes))
+
+
+if __name__ == "__main__":
+    main()
+
