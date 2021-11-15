575a443b6f59f90962d9d3a7cc7ce696d386febc
Stefan Arentz <stefan@arentz.ca>
2021-11-14 19:34:57 -0500

    2015 Day 20

--- 2015/day20.c
+++ 2015/day20.c
@@ -12,7 +12,7 @@
 #define NUM_HOUSES 10000000
 #define BATCH_SIZE 50000
 
-int number_of_presents(int house) {
+int number_of_presents1(int house) {
   int n = 0;
   for (int elf = 1; elf <= house; elf++) {
     if ((house % elf) == 0) {
@@ -22,7 +22,7 @@ int number_of_presents(int house) {
   return n;
 }
 
-int main() {
+void part1() {
   dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
   dispatch_group_t group = dispatch_group_create();
 
@@ -32,7 +32,7 @@ int main() {
     dispatch_group_async(group, queue, ^{
       int start = i * BATCH_SIZE;
       for (int house = start; house <= start + BATCH_SIZE - 1; house++) {
-        int n = number_of_presents(house);
+        int n = number_of_presents1(house);
         if (n >= INPUT) {
           dispatch_async(queue, ^{
             if (house < lowest_house) {
@@ -51,3 +51,48 @@ int main() {
   printf("Part one: %d\n", lowest_house);
 }
 
+int number_of_presents2(int house) {
+  int n = 0;
+  for (int elf = 1; elf <= house; elf++) {
+    if ((house % elf) == 0) {
+      if (house / elf < 50) {
+        n += (11 * elf);
+      }
+    }
+  }
+  return n;
+}
+
+void part2() {
+  dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+  dispatch_group_t group = dispatch_group_create();
+
+  __block int lowest_house = NUM_HOUSES;
+
+  for (int i = 0; i < (NUM_HOUSES / BATCH_SIZE); i++) {
+    dispatch_group_async(group, queue, ^{
+      int start = i * BATCH_SIZE;
+      for (int house = start; house <= start + BATCH_SIZE - 1; house++) {
+        int n = number_of_presents2(house);
+        if (n >= INPUT) {
+          dispatch_async(queue, ^{
+            if (house < lowest_house) {
+              lowest_house = house;
+            }
+          });
+          return;
+        }
+      }
+    });
+  }
+
+  dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
+  dispatch_release(group);
+
+  printf("Part two: %d\n", lowest_house);
+}
+
+int main() {
+  //part1();
+  part2();
+}
