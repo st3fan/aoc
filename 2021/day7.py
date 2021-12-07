af401d3f5b791901bf88bfd8e4a665c9288e6e1b
Stefan Arentz <stefan@arentz.ca>
2021-12-07 08:57:15 -0500

    2021 Day 7

---
+++ 2021/day7.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+
+def load():
+    return [int(e) for e in open("day7.input").readline().split(",")]
+
+
+def fuel_spent1(start_positions, end_position):
+    total = 0
+    for p in start_positions:
+        total += abs(p - end_position)
+    return total
+
+
+def part1():
+    start_positions = load()
+    return min(fuel_spent1(start_positions, pos) for pos in range(5000))
+
+
+# See https://en.wikipedia.org/wiki/Summation
+def triangle_number(n):
+    return ((n * n) + n) // 2
+
+
+def fuel_spent2(start_positions, end_position):
+    total = 0
+    for p in start_positions:
+        total += triangle_number(abs(p - end_position))
+    return total
+
+
+def part2():
+    start_positions = load()
+    return min(fuel_spent2(start_positions, pos) for pos in range(5000))
+
+
+if __name__ == "__main__":
+    print("Part one:", part1())
+    print("Part two:", part2())
+
