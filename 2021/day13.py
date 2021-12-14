c2fc474f764c8387480b0fb53f71134c3e81954c
Stefan Arentz <stefan@arentz.ca>
2021-12-13 19:24:36 -0500

    2021 Day 13

--- 2021/day13.py
+++ 2021/day13.py
@@ -33,26 +33,10 @@ def count_pixels(image):
 
 
 def fold_horizontally(image, y):
-    print("o pixels:", count_pixels(image))
-
     a = image.transform((image.width, y), Image.EXTENT, (0, 0, image.width, y)).transpose(Image.FLIP_TOP_BOTTOM)
-    print("a: box:", (0, 0, image.width-1, y-1))
-
     b = image.transform((image.width, image.height-y-1), Image.EXTENT, (0, y+1, image.width, image.height))
-    print("b: box:", (0, y+1, image.width-1, image.height-1))
-
-    print("a: size:", a.width, a.height)
-    print("b: size:", b.width, b.height)
-
-    print("a pixels:", count_pixels(a))
-    print("b pixels:", count_pixels(b))
-
     assert b.height <= a.height
-
     a.alpha_composite(b, dest=(0, a.height-b.height))
-
-    print("c pixels:", count_pixels(a))
-
     return a
 
 
@@ -63,7 +47,28 @@ def fold_vertically(image, x):
     a.alpha_composite(b, dest=(a.width-b.width, 0))
     return a
 
+
 def part1():
+    pixels = load_pixels()
+
+    width = max(p[0] for p in pixels)+1
+    height = max(p[1] for p in pixels)+1
+
+    image = Image.new("RGBA", (width, height), color=(0,0,0,0))
+    for p in pixels:
+        image.putpixel(p, (255,0,0))
+
+    folds = list(load_folds())
+    match folds[0]:
+        case ("x", int(x)):
+            image = fold_vertically(image, x)
+        case ("y", int(y)):
+            image = fold_horizontally(image, y)
+
+    return count_pixels(image)
+
+
+def part2():
     pixels = load_pixels()
 
     width = max(p[0] for p in pixels)+1
@@ -76,18 +81,16 @@ def part1():
     for i, f in enumerate(load_folds()):
         match f:
             case ("x", int(x)):
-                print(f"Folding vertically at {x}")
                 image = fold_vertically(image, x)
             case ("y", int(y)):
-                print(f"Folding horizontally at {y}")
                 image = fold_horizontally(image, y)
 
-        image.save(f"day13-{i}.png")
-        print(f"After step {i}: {count_pixels(image)} pixels")
+    image.transpose(Image.ROTATE_180).save("day13.png", "PNG")
 
-    return count_pixels(image)
+    return "see day13.png"
 
 
 if __name__ == "__main__":
-    print("Part one:", part1(), "(Should be 17)")
+    print("Part one:", part1())
+    print("Part two:", part2())
 
