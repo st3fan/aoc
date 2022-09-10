14217b5a2b60f4bceeb93705052c35151ca3a1ef
Stefan Arentz <stefan@arentz.ca>
2022-09-09 20:48:43 -0400

    Updates

---
+++ 2021/ex/sonar_sweep/lib/sonar_sweep.ex
@@ -0,0 +1,26 @@
+defmodule SonarSweep do
+
+  def read_input!(path) do
+    File.read!(path)
+    |> String.trim()
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def part1() do
+    read_input!("assets/input.txt")
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.count(fn [a,b] -> a < b end)
+  end
+
+  def part2() do
+    read_input!("assets/input.txt")
+    |> Enum.chunk_every(3, 1, :discard)
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.count(fn [a, b] -> Enum.sum(b) > Enum.sum(a) end)
+  end
+
+end
+
+# 1 2 3
+#   2 3 4
---
+++ 2021/ex/sonar_sweep/test/sonar_sweep_test.exs
@@ -0,0 +1,12 @@
+defmodule SonarSweepTest do
+  use ExUnit.Case
+  doctest SonarSweep
+
+  test "part1" do
+    assert SonarSweep.part1() == 1532
+  end
+
+  test "part2" do
+    assert SonarSweep.part2() == 1571
+  end
+end
