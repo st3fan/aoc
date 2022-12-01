b0f77cbbcab58bbfdfa90031edf46a947b139ac2
Stefan Arentz <stefan@arentz.ca>
2022-12-01 08:02:04 -0500

    2022, Day 1, Python

---
+++ 2022/py/day1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+
+from typing import Generator, List
+
+
+def read_input() -> Generator[List[int], None, None]:
+    for group in open("day1.txt").read().split("\n\n"):
+        yield [int(line) for line in group.strip().split("\n")]
+
+
+def part1():
+    return max(
+        sum(group) for group in read_input()
+    )
+
+
+def part2():
+    return sum(
+        sorted(
+            (sum(group) for group in read_input()),
+            reverse=True
+        )[:3]
+    )
+
+
+if __name__ == "__main__":
+    print("Part1:", part1())
+    print("Part2:", part2())
