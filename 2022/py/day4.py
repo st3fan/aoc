4205f1bccae46ae784f6f4b2e3425950a9d2970c
Stefan Arentz <stefan@arentz.ca>
2022-12-06 19:33:25 -0500

    2022, Day 4, Python (Formatting)

--- 2022/py/day4.py
+++ 2022/py/day4.py
@@ -11,12 +11,15 @@ class Range:
     end: int
 
     def contains(self: Self, o: Self) -> bool:
-        return self.start >= o.start and self.end <= o.end \
-            or o.start >= self.start and o.end <= self.end
+        return self.start >= o.start and self.end <= o.end or o.start >= self.start and o.end <= self.end
 
     def intersects(self: Self, o: Self) -> bool:
-        return self.start <= o.start <= self.end or self.start <= o.end <= self.end \
-            or o.start <= self.start <= o.end or o.start <= self.end <= o.end
+        return (
+            self.start <= o.start <= self.end
+            or self.start <= o.end <= self.end
+            or o.start <= self.start <= o.end
+            or o.start <= self.end <= o.end
+        )
 
     @classmethod
     def from_string(cls, s: str) -> Self:
@@ -42,15 +45,11 @@ def read_input() -> Generator[Pair, None, None]:
 
 
 def part1() -> int:
-    return sum(
-        pair.a.contains(pair.b) or pair.b.contains(pair.a) for pair in read_input()
-    )
+    return sum(pair.a.contains(pair.b) or pair.b.contains(pair.a) for pair in read_input())
 
 
 def part2() -> int:
-    return sum(
-        pair.a.intersects(pair.b) for pair in read_input()
-    )
+    return sum(pair.a.intersects(pair.b) for pair in read_input())
 
 
 if __name__ == "__main__":
