42bc3a15b9c6e81c1bbfac3a68b9c6e69a994431
Stefan Arentz <stefan@arentz.ca>
2022-12-10 16:57:01 -0500

    2022, Day 10, Python

--- 2022/py/day10.py
+++ 2022/py/day10.py
@@ -2,51 +2,40 @@
 
 
 from itertools import cycle
-from more_itertools import nth, take, chunked
-from typing import Generator, List, Tuple
+from more_itertools import take, chunked
+from typing import Generator, List
 
 
 def read_input() -> List[None | int]:
     def _parse_line(line: str) -> None | int:
-        if line.startswith("addx"):
+        if line.startswith("addx "):
             return int(line[5:])
 
     return [_parse_line(line.strip()) for line in open("day10.txt").readlines()]
 
 
-def signal_generator(instructions: list[None | int]) -> Generator[Tuple[int, int, int], None, None]:
-    c: int = 0
-    x: int = 1
+def beam_cycle_values(instructions: list[None | int]) -> Generator[int, None, None]:
     for instruction in cycle(instructions):
         match instruction:
             case None:
-                c += 1
-                yield (c, x, c * x)
-            case int(v):
-                c += 1
-                yield (c, x, c * x)
-                c += 1
-                yield (c, x, c * x)
-                x += v
-
-
-def beam_movement_generator(instructions: list[None | int]) -> Generator[int | None, None, None]:
-    for instruction in cycle(instructions):
-        match instruction:
-            case None:
-                yield None
+                yield 0
             case int(dx):
-                yield None
+                yield 0
                 yield dx
 
 
 def part1() -> int:
-    g = signal_generator(read_input())
-    return sum(e[2] for e in take(220, g) if e[0] in (20, 60, 100, 140, 180, 220))
+    t: int = 0
+    x: int = 1
+    for cycle, dx in enumerate(take(220, beam_cycle_values(read_input())), start=1):
+        if cycle in (20, 60, 100, 140, 180, 220):
+            t += cycle * x
+        x += dx
+    return t
 
 
 def part2() -> int:
-    g = beam_movement_generator(read_input())
+    g = beam_cycle_values(read_input())
     rows = [[" "] * 40 for _ in range(6)]
 
     sx: int = 1
@@ -54,8 +43,7 @@ def part2() -> int:
         for (x, xd) in enumerate(take(40, g)):
             if x in (sx - 1, sx, sx + 1):
                 rows[r][x] = "#"
-            if xd != None:
-                sx += xd
+            sx += xd
 
     for r in rows:
         line = ""
