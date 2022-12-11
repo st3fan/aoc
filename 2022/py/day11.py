6d793217db668a184c1da828efcc3ae11e138088
Stefan Arentz <stefan@arentz.ca>
2022-12-11 14:33:53 -0500

    2022, Day 11, Python

--- 2022/py/day11.py
+++ 2022/py/day11.py
@@ -2,16 +2,13 @@
 
 
 from dataclasses import dataclass
+from math import prod
 from typing import Callable, List, Self
 
 
 @dataclass
 class Item:
     worry_level: int
-
-
-DIVISORS_MUL = 11 * 7 * 13 * 5 * 3 * 17 * 2 * 19
-# DIVISORS_MUL = 23 * 19 * 13 * 17
 
 
 @dataclass
@@ -26,74 +23,55 @@ class Monkey:
     def __lt__(self, other: Self) -> bool:
         return self.inspections < other.inspections
 
-    def inspect_item1(self, item: Item) -> int:
+    def inspect_item(self, item: Item, modulus: int) -> int:
         self.inspections += 1
         item.worry_level = self.operation(item.worry_level)
-        item.worry_level //= 3
+        if modulus != 0:
+            item.worry_level %= modulus
+        else:
+            item.worry_level //= 3
         if (item.worry_level % self.divisor) == 0:
             return self.true_monkey
         else:
             return self.false_monkey
 
-    def inspect_item2(self, item: Item) -> int:
-        self.inspections += 1
-        item.worry_level = self.operation(item.worry_level)
-        item.worry_level %= DIVISORS_MUL
-        # item.worry_level //= 3
-        if (item.worry_level % self.divisor) == 0:
-            return self.true_monkey
-        else:
-            return self.false_monkey
+
+def read_input() -> List[Monkey]:
+    return [
+        # Monkey([Item(w) for w in [79, 98]], lambda old: old * 19, 23, 2, 3),
+        # Monkey([Item(w) for w in [54, 65, 75, 74]], lambda old: old + 6, 19, 2, 0),
+        # Monkey([Item(w) for w in [79, 60, 97]], lambda old: old * old, 13, 1, 3),
+        # Monkey([Item(w) for w in [74]], lambda old: old + 3, 17, 0, 1),
+        Monkey([Item(w) for w in [57]], lambda old: old * 13, 11, 3, 2),
+        Monkey([Item(w) for w in [58, 93, 88, 81, 72, 73, 65]], lambda old: old + 2, 7, 6, 7),
+        Monkey([Item(w) for w in [65, 95]], lambda old: old + 6, 13, 3, 5),
+        Monkey([Item(w) for w in [58, 80, 81, 83]], lambda old: old * old, 5, 4, 5),
+        Monkey([Item(w) for w in [58, 89, 90, 96, 55]], lambda old: old + 3, 3, 1, 7),
+        Monkey([Item(w) for w in [66, 73, 87, 58, 62, 67]], lambda old: old * 7, 17, 4, 1),
+        Monkey([Item(w) for w in [85, 55, 89]], lambda old: old + 4, 2, 2, 0),
+        Monkey([Item(w) for w in [73, 80, 54, 94, 90, 52, 69, 58]], lambda old: old + 7, 19, 6, 0),
+    ]
+
+
+def _part(monkeys: List[Monkey], rounds: int, modulus: int = 0) -> int:
+    for _ in range(rounds):
+        for monkey in monkeys:
+            for item in monkey.items.copy():
+                next_monkey = monkey.inspect_item(item, modulus)
+                monkey.items.remove(item)
+                monkeys[next_monkey].items.append(item)
+    monkeys.sort(reverse=True)
+    return monkeys[0].inspections * monkeys[1].inspections
 
 
 def part1() -> int:
-    monkeys = [
-        # Monkey([Item(w) for w in [79, 98]], lambda old: old * 19, 23, 2, 3),
-        # Monkey([Item(w) for w in [54, 65, 75, 74]], lambda old: old + 6, 19, 2, 0),
-        # Monkey([Item(w) for w in [79, 60, 97]], lambda old: old * old, 13, 1, 3),
-        # Monkey([Item(w) for w in [74]], lambda old: old + 3, 17, 0, 1),
-        Monkey([Item(w) for w in [57]], lambda old: old * 13, 11, 3, 2),
-        Monkey([Item(w) for w in [58, 93, 88, 81, 72, 73, 65]], lambda old: old + 2, 7, 6, 7),
-        Monkey([Item(w) for w in [65, 95]], lambda old: old + 6, 13, 3, 5),
-        Monkey([Item(w) for w in [58, 80, 81, 83]], lambda old: old * old, 5, 4, 5),
-        Monkey([Item(w) for w in [58, 89, 90, 96, 55]], lambda old: old + 3, 3, 1, 7),
-        Monkey([Item(w) for w in [66, 73, 87, 58, 62, 67]], lambda old: old * 7, 17, 4, 1),
-        Monkey([Item(w) for w in [85, 55, 89]], lambda old: old + 4, 2, 2, 0),
-        Monkey([Item(w) for w in [73, 80, 54, 94, 90, 52, 69, 58]], lambda old: old + 7, 19, 6, 0),
-    ]
-    for _ in range(20):
-        for monkey in monkeys:
-            for item in monkey.items.copy():
-                next_monkey = monkey.inspect_item1(item)
-                monkey.items.remove(item)
-                monkeys[next_monkey].items.append(item)
-    monkeys.sort(reverse=True)
-    return monkeys[0].inspections * monkeys[1].inspections
+    monkeys = read_input()
+    return _part(monkeys, 20)
 
 
 def part2() -> int:
-    monkeys = [
-        # Monkey([Item(w) for w in [79, 98]], lambda old: old * 19, 23, 2, 3),
-        # Monkey([Item(w) for w in [54, 65, 75, 74]], lambda old: old + 6, 19, 2, 0),
-        # Monkey([Item(w) for w in [79, 60, 97]], lambda old: old * old, 13, 1, 3),
-        # Monkey([Item(w) for w in [74]], lambda old: old + 3, 17, 0, 1),
-        Monkey([Item(w) for w in [57]], lambda old: old * 13, 11, 3, 2),
-        Monkey([Item(w) for w in [58, 93, 88, 81, 72, 73, 65]], lambda old: old + 2, 7, 6, 7),
-        Monkey([Item(w) for w in [65, 95]], lambda old: old + 6, 13, 3, 5),
-        Monkey([Item(w) for w in [58, 80, 81, 83]], lambda old: old * old, 5, 4, 5),
-        Monkey([Item(w) for w in [58, 89, 90, 96, 55]], lambda old: old + 3, 3, 1, 7),
-        Monkey([Item(w) for w in [66, 73, 87, 58, 62, 67]], lambda old: old * 7, 17, 4, 1),
-        Monkey([Item(w) for w in [85, 55, 89]], lambda old: old + 4, 2, 2, 0),
-        Monkey([Item(w) for w in [73, 80, 54, 94, 90, 52, 69, 58]], lambda old: old + 7, 19, 6, 0),
-    ]
-    for _ in range(10000):
-        for monkey in monkeys:
-            for item in monkey.items.copy():
-                next_monkey = monkey.inspect_item2(item)
-                monkey.items.remove(item)
-                monkeys[next_monkey].items.append(item)
-    monkeys.sort(reverse=True)
-    return monkeys[0].inspections * monkeys[1].inspections
+    monkeys = read_input()
+    return _part(monkeys, 10000, prod(m.divisor for m in monkeys))
 
 
 if __name__ == "__main__":
