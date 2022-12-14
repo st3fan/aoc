920976857a5076e99cb67b79f1059bf480f9eced
Stefan Arentz <stefan@arentz.ca>
2022-12-13 20:15:39 -0500

    2022, Day 12, Python

--- 2022/py/day12.py
+++ 2022/py/day12.py
@@ -1,66 +1,73 @@
 #!/usr/bin/env python
 
-from typing import Generator, List
+
+from typing import Dict, Generator, List, Tuple, cast
 from aoc import Grid, Position
 
 
-def read_input() -> Grid:
-    return Grid.from_file("day12.txt")
+def read_input() -> Tuple[Grid[int], Position, Position]:
+    grid = Grid.from_file("day12.txt", lambda v: v)
+    start_position = cast(Position, grid.find("S"))
+    end_position = cast(Position, grid.find("E"))
+
+    def _transform(v: str) -> int:
+        match v:
+            case "S":
+                return ord("a")
+            case "E":
+                return ord("z")
+            case _:
+                return ord(v)
+
+    return (Grid.from_file("day12.txt", _transform), start_position, end_position)
 
 
-def _can_move(grid: Grid, src_position: Position, dst_position: Position, path: List[Position]) -> bool:
-    if dst_position in path:
-        return False
-
-    src: int = ord(grid.get(src_position))
-    dst: int = ord(grid.get(dst_position))
-
-    if src == ord("S"):
-        src = ord("a")
-
-    if dst == ord("E"):
-        dst = ord("z")
-
-    if dst <= src:
-        return True
-
-    if dst > src and ((dst - src) == 1):
-        return True
-
-    return False
+def _can_move(grid: Grid[int], src_position: Position, dst_position: Position) -> bool:
+    src: int = grid.get(src_position)
+    dst: int = grid.get(dst_position)
+    return dst <= src or (dst > src and ((dst - src) == 1))
 
 
-def find_all_paths(grid: Grid, position: Position, path: List[Position] = []) -> Generator[List[Position], None, None]:
-    path.append(position)
-    if grid.get(position) == "E":
-        # print("Got to the end!")
-        yield path
-    else:
-        # print("Loop:")
-        for neighbour_position in grid.neighbours(position):
-            # print("Looking at", neighbour_position, path)
-            if _can_move(grid, position, neighbour_position, path):
-                # print("  Can go", neighbour_position)
-                yield from find_all_paths(grid, neighbour_position, path)
-            # else:
-            # print("  Can't go")
-    path.pop()
+def bfs(grid: Grid[int], start_position: Position, end_position: Position) -> List[Position]:
+    queue: List[Position] = []
+    visited: Dict[Position, Position | None] = {start_position: None}
+
+    current = start_position
+    while current != end_position:
+        for neighbour in grid.neighbours(current):
+            if neighbour not in visited:
+                if _can_move(grid, current, neighbour):
+                    visited[neighbour] = current
+                    queue.append(neighbour)
+        if len(queue) == 0:
+            return []
+        current = queue.pop(0)
+
+    path: List[Position] = [current]
+
+    while current != start_position:
+        current = visited[current]
+        path.insert(0, current)
+
+    return path
 
 
 def part1() -> int:
-    grid = Grid.from_file("day12.txt")
+    grid, start_position, end_position = read_input()
+    return len(bfs(grid, start_position, end_position)) - 1
 
-    # for path in find_all_paths(grid, grid.find("S")):  # type: ignore
-    #     print(path)
 
-    for _ in range(1000):
-        min(len(path) for path in find_all_paths(grid, grid.find("S")))
-
-    return min(len(path) for path in find_all_paths(grid, grid.find("S"))) - 1  # type: ignore
+def _lowest_positions(grid: Grid[int]) -> Generator[Position, None, None]:
+    for y in range(grid.height):
+        for x in range(grid.width):
+            p = Position(x, y)
+            if grid.get(p) == ord("a"):
+                yield p
 
 
 def part2() -> int:
-    return 0
+    grid, _, end_position = read_input()
+    return min([len(path) for path in [bfs(grid, p, end_position) for p in _lowest_positions(grid)] if path]) - 1
 
 
 if __name__ == "__main__":
