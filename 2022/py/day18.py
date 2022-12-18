347b44aa5b5165e7b7e05ee4757e059a21866547
Stefan Arentz <stefan@arentz.ca>
2022-12-18 18:00:29 -0500

    2022, Day 18, Python

---
+++ 2022/py/day18.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+
+from dataclasses import dataclass
+from typing import List, Self
+
+
+@dataclass(frozen=True)
+class Cube:
+    x: int
+    y: int
+    z: int
+
+    @classmethod
+    def from_str(cls, s: str) -> Self:
+        c = s.split(",")
+        return cls(int(c[0]), int(c[1]), int(c[2]))
+
+    def adjecents(self) -> List[Self]:
+        return [
+            Cube(self.x - 1, self.y, self.z),
+            Cube(self.x + 1, self.y, self.z),
+            Cube(self.x, self.y - 1, self.z),
+            Cube(self.x, self.y + 1, self.z),
+            Cube(self.x, self.y, self.z - 1),
+            Cube(self.x, self.y, self.z + 1),
+        ]
+
+
+def read_input() -> List[Cube]:
+    return [Cube.from_str(line.strip()) for line in open("day18.txt").readlines()]
+
+
+def part1() -> int:
+    cubes: set[Cube] = set(read_input())
+    total = 0
+    for cube in cubes:
+        for adj in cube.adjecents():
+            if adj not in cubes:
+                total += 1
+    return total
+
+
+def part2() -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    print("Part 1:", part1())
+    print("Part 2:", part2())
