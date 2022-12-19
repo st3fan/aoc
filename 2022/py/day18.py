e6f053755262cfbdd018e1f1c6fca74fbf5a4208
Stefan Arentz <stefan@arentz.ca>
2022-12-18 20:07:29 -0500

    2022, Day 18, Python

--- 2022/py/day18.py
+++ 2022/py/day18.py
@@ -2,11 +2,34 @@
 
 
 from dataclasses import dataclass
-from typing import List, Self, Set
+from typing import List, NamedTuple, Self, Set
+
+import numpy as np
+from scipy import ndimage
+
+# @dataclass(frozen=True)
+# class Cube:
+#     x: int
+#     y: int
+#     z: int
+
+#     @classmethod
+#     def from_str(cls, s: str) -> Self:
+#         c = s.split(",")
+#         return cls(int(c[0]), int(c[1]), int(c[2]))
+
+#     def adjecents(self) -> List[Self]:
+#         return [
+#             Cube(self.x - 1, self.y, self.z),
+#             Cube(self.x + 1, self.y, self.z),
+#             Cube(self.x, self.y - 1, self.z),
+#             Cube(self.x, self.y + 1, self.z),
+#             Cube(self.x, self.y, self.z - 1),
+#             Cube(self.x, self.y, self.z + 1),
+#         ]
 
 
-@dataclass(frozen=True)
-class Cube:
+class Cube(NamedTuple):
     x: int
     y: int
     z: int
@@ -45,7 +68,26 @@ def part1() -> int:
 
 
 def part2() -> int:
-    return 0
+    cubes: Set[Cube] = read_input()
+
+    mx = max(c.x for c in cubes) + 1
+    my = max(c.y for c in cubes) + 1
+    mz = max(c.z for c in cubes) + 1
+
+    array = np.zeros((mx, my, mz), dtype=int)
+    for cube in cubes:
+        array[cube] = 1
+
+    inside = ndimage.binary_fill_holes(array).astype(int) - array
+
+    inside_cubes: Set[Cube] = set()
+    for x in range(mx):
+        for y in range(my):
+            for z in range(mz):
+                if inside[x, y, z] != 0:
+                    inside_cubes.add(Cube(x, y, z))
+
+    return part1() - outer_surface(inside_cubes)
 
 
 if __name__ == "__main__":
