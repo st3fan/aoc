5e0414582f94b22baf9a8bad5ba1f5a3dd034da1
Stefan Arentz <stefan@arentz.ca>
2023-12-15 16:47:33 -0500

    2023 Python Day 15

--- 2023/py/day15.py
+++ 2023/py/day15.py
@@ -5,6 +5,7 @@ import re
 
 from collections import OrderedDict
 from dataclasses import dataclass, field
+from functools import reduce
 from typing import List, OrderedDict
 
 
@@ -13,16 +14,10 @@ def read_input() -> List[str]:
 
 
 def hash_string(s: str) -> int:
-    t = 0
-    for c in s:
-        t += ord(c)
-        t *= 17
-        t %= 256
-    return t
+    return reduce(lambda a, c: ((a + ord(c)) * 17) % 256, s, 0)
 
 
 def part1() -> int:
-    # return hash_string("HASH")
     return sum(hash_string(s) for s in read_input())
 
 
@@ -32,10 +27,10 @@ class Box:
     lenses: OrderedDict[str, int] = field(default_factory=OrderedDict)
 
     def focal_power(self) -> int:
-        t = 0
-        for slot, (lens_label, focal_length) in enumerate(self.lenses.items(), 1):
-            t += (self.id + 1) * slot * focal_length
-        return t
+        return sum(
+            (self.id + 1) * slot * focal_length
+            for slot, focal_length in enumerate(self.lenses.values(), 1)
+        )
 
     def add_lens(self, label: str, focal_length: int):
         self.lenses[label] = focal_length
@@ -43,28 +38,16 @@ class Box:
     def remove_lens(self, label: str):
         if label in self.lenses:
             del self.lenses[label]
-        # if label in self.lenses:
-        #     keys = list(self.lenses.keys())
-        #     for key in keys[keys.index(label) :]:
-        #         del self.lenses[key]
 
 
 def part2() -> int:
     boxes = [Box(i) for i in range(0, 256)]
-
     for s in read_input():
         match re.findall(r"(\w+|-|=)", s):
             case [label, "-"]:
                 boxes[hash_string(label)].remove_lens(label)
             case [label, "=", focal_length]:
                 boxes[hash_string(label)].add_lens(label, int(focal_length))
-
-    # boxes[0].lenses["a"] = 1
-    # boxes[0].lenses["b"] = 2
-    # boxes[3].lenses["a"] = 7
-    # boxes[3].lenses["b"] = 5
-    # boxes[3].lenses["c"] = 6
-
     return sum(box.focal_power() for box in boxes)
 
 
