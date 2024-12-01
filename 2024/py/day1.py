d1d92fa72bfa58809e269772fa477c564997a18f
Stefan Arentz <stefan@arentz.ca>
2024-12-01 10:08:30 -0500

    2024 Python Day 1

--- 2024/py/day1.py
+++ 2024/py/day1.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
 if __name__ == "__main__":
-    left, right = list(
-        zip(*[(int(a), int(b)) for a, b in [line.split() for line in open("day1.txt")]])
-    )
+    left, right = list(zip(*[(int(a), int(b)) for a, b in [line.split() for line in open("day1.txt")]]))
     print("Part 1:", sum(abs(a - b) for a, b in zip(sorted(left), sorted(right))))
     print("Part 2:", sum((a * right.count(a)) for a in left))
