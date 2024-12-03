ee72d4356dad4e03f2b42103ae37f46d0ee7de25
Stefan Arentz <stefan@arentz.ca>
2024-12-02 20:15:14 -0500

    2024 Python Day 2

--- 2024/py/day2.py
+++ 2024/py/day2.py
@@ -11,26 +11,10 @@ def read_input(path):
 
 
 def check(report) -> bool:
-    if report[0] == report[1]:
-        return False
-
-    if report[0] > report[1]:
-        for a, b in pairwise(report):
-            if a <= b:
-                return False
-            d = abs(a - b)
-            if d < 1 or d > 3:
-                return False
-
-    if report[0] < report[1]:
-        for a, b in pairwise(report):
-            if a >= b:
-                return False
-            d = abs(a - b)
-            if d < 1 or d > 3:
-                return False
-
-    return True
+    for a, b in pairwise(report):
+        if not (1 <= abs(a - b) <= 3):
+            return False
+    return not sorted(report, reverse=(report[0] > report[1])) != report
 
 
 def dampened_check(report):
