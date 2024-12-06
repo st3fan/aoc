938d97af4925245bcdf9b2d29a413149458b73c3
Stefan Arentz <stefan@arentz.ca>
2024-12-06 18:51:58 -0500

    2024 Python Day 6

--- 2024/py/day6.py
+++ 2024/py/day6.py
@@ -41,8 +41,42 @@ def part1():
     return len(positions)
 
 
+def stuck(map):
+    v = (0, -1)  # Up
+
+    positions = {}  # (position, v)
+
+    if p := map.find("^"):  # Conditional to keep mypy happy
+        while True:
+            positions[p] = 1
+            np = Position(p.x + v[0], p.y + v[1])
+            if np.x < 0 or np.y < 0 or np.x == map.width or np.y == map.height:
+                break  # We fell off the map
+            if map.get(np) == "#":
+                # We hit a wall and turned
+                v = DIRECTION_VECTOR_CHANGES[v]
+            else:
+                key = (np, v)
+                if key in positions:
+                    return True
+                # We actually moved
+                p = np
+                positions[key] = 1
+    return False
+
+
 def part2():
-    pass
+    map = Grid.from_file("day6.txt", lambda v: v)
+    total = 0
+    for y in range(map.height):
+        for x in range(map.width):
+            p = Position(x, y)
+            if map.get(p) == ".":
+                map.set(p, "#")
+                if stuck(map):
+                    total += 1
+                map.set(p, ".")
+    return total
 
 
 if __name__ == "__main__":
