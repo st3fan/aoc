bfedaf1fcfc083773fa6ab70084792c196d13f20
Stefan Arentz <stefan@arentz.ca>
2024-12-07 10:10:23 -0500

    2024 Python Day 7

--- 2024/py/day7.py
+++ 2024/py/day7.py
@@ -10,41 +10,20 @@ def read_input(path):
             yield int(tn), [v for v in en.split(" ")]
 
 
-def concat_numbers(input_list):
-    output_list = []
-    i = 0
-    while i < len(input_list):
-        if input_list[i] == "||":
-            output_list[-1] += input_list[i + 1]
-            i += 2
-        else:
-            output_list.append(input_list[i])
-            i += 1
-    return output_list
-
-
 def left_to_right(tokens):
+    """Wrap in parens so that the expression is evaluated left to right"""
     e = tokens[0]
     it = iter(tokens[1:])
     for o, v in zip_longest(it, it, fillvalue=None):
         if o == "||":
-            e = "(" + f"int(str({e}) + '{v}')" + ")"  # Mind exploding
+            e = "(" + f"int(str({e}) + '{v}')" + ")"
         else:
             e = "(" + e + o + v + ")"
     return e
 
 
-def solve2(value, numbers):
-    for operators in product(["*", "+", "||"], repeat=len(numbers) - 1):
-        tokens = [item for pair in zip_longest(numbers, operators) for item in pair if item is not None]
-        tokens = left_to_right(tokens)
-        if eval(tokens) == value:
-            return value
-    return 0
-
-
-def solve1(value, numbers):
-    for operators in product(["*", "+"], repeat=len(numbers) - 1):
+def solve(value, numbers, tokens):
+    for operators in product(tokens, repeat=len(numbers) - 1):
         tokens = [item for pair in zip_longest(numbers, operators) for item in pair if item is not None]
         expression = left_to_right(tokens)
         if eval(expression) == value:
@@ -53,5 +32,5 @@ def solve1(value, numbers):
 
 
 if __name__ == "__main__":
-    print("Part1:", sum(solve1(value, numbers) for value, numbers in read_input("day7.txt")))
-    print("Part2:", sum(solve2(value, numbers) for value, numbers in read_input("day7.txt")))
+    print("Part1:", sum(solve(value, numbers, ("+", "*")) for value, numbers in read_input("day7.txt")))
+    print("Part2:", sum(solve(value, numbers, ("+", "*", "||")) for value, numbers in read_input("day7.txt")))
