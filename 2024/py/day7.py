909e95a9ea14abf51ab5dfb6e32f087cd7cab316
Stefan Arentz <stefan@arentz.ca>
2024-12-07 17:19:11 -0500

    2024 Python Day 7

--- 2024/py/day7.py
+++ 2024/py/day7.py
@@ -2,13 +2,14 @@
 
 
 from itertools import product
+from multiprocessing import Pool
 
 
 def read_input(path):
     with open(path) as fp:
         for line in fp.readlines():
             tn, en = line.strip().split(": ")
-            yield int(tn), [int(v) for v in en.split(" ")]
+            yield (int(tn), [int(v) for v in en.split(" ")])
 
 
 def solve(value, numbers, tokens):
@@ -29,6 +30,19 @@ def solve(value, numbers, tokens):
     return 0
 
 
+def process_chunk(input):
+    return sum(solve(value, numbers, ("+", "*", "||")) for value, numbers in input)
+
+
+def part2(input):
+    n = 6  # MacBook Air with 6 Performance cores
+    chunks = [input[i : i + n] for i in range(0, len(input), n)]
+    with Pool(processes=n) as pool:
+        results = pool.map(process_chunk, chunks)
+        return sum(results)
+
+
 if __name__ == "__main__":
-    print("Part1:", sum(solve(value, numbers, ("+", "*")) for value, numbers in read_input("day7.txt")))
-    print("Part2:", sum(solve(value, numbers, ("+", "*", "||")) for value, numbers in read_input("day7.txt")))
+    print("Part1:", sum(solve(value, numbers, ("+", "*")) for (value, numbers) in read_input("day7.txt")))
+    # print("Part2:", sum(solve(value, numbers, ("+", "*", "||")) for value, numbers in read_input("day7.txt")))
+    print("Part2:", part2(list(read_input("day7.txt"))))
