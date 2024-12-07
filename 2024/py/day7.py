8356090ef69ec102cf6d6b30b17c6d57f6220676
Stefan Arentz <stefan@arentz.ca>
2024-12-07 11:06:38 -0500

    2024 Python Day 7

--- 2024/py/day7.py
+++ 2024/py/day7.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-from itertools import product, zip_longest
+
+from itertools import product
 
 
 def read_input(path):
@@ -10,30 +11,24 @@ def read_input(path):
             yield int(tn), [int(v) for v in en.split(" ")]
 
 
-def evaluate(tokens, value):
-    a: int = tokens[0]
-    it = iter(tokens[1:])
-    for o, v in zip_longest(it, it, fillvalue=None):
-        if a > value:
-            return False
-        match o, v:
-            case "+", v:
-                a += v  # pyright: ignore
-            case "*", v:
-                a *= v  # pyright: ignore
-            case "||", v:
-                a = int(str(a) + str(v))
-    return a
-
-
 def solve(value, numbers, tokens):
     for operators in product(tokens, repeat=len(numbers) - 1):
-        tokens = [item for pair in zip_longest(numbers, operators) for item in pair if item is not None]
-        if evaluate(tokens, value) == value:
-            return value
+        a = numbers[0]
+        for o, v in zip(operators, numbers[1:]):
+            match o, v:
+                case "+", v:
+                    a += v  # pyright: ignore
+                case "*", v:
+                    a *= v  # pyright: ignore
+                case "||", v:
+                    a = int(str(a) + str(v))
+            if a > value:
+                break
+            if a == value:
+                return a
     return 0
 
 
 if __name__ == "__main__":
-    print("Part1:", sum(solve(value, numbers, ("+", "*", "||")) for value, numbers in read_input("day7.txt")))
+    print("Part1:", sum(solve(value, numbers, ("+", "*")) for value, numbers in read_input("day7.txt")))
     print("Part2:", sum(solve(value, numbers, ("+", "*", "||")) for value, numbers in read_input("day7.txt")))
