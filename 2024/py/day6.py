d902f7ce3e8cbb85daae92dc674c399ed89fb59d
Stefan Arentz <stefan@arentz.ca>
2024-12-06 19:38:45 -0500

    2024 Python Day 6 (Optimized)

--- 2024/py/day6.py
+++ 2024/py/day6.py
@@ -27,7 +27,7 @@ def profile(fnc):
 
 def load_input(path):
     with open(path) as fp:
-        return [list(line.strip()) for line in fp.readlines()]
+        return [[ord(c) for c in line.strip()] for line in fp.readlines()]
 
 
 # Map from direction to new direction if a wall is hit
@@ -55,13 +55,13 @@ def part1():
 
     positions = {}
 
-    if p := find(map, "^"):  # Conditional to keep mypy happy
+    if p := find(map, 94):  # Conditional to keep mypy happy
         while True:
             positions[p] = 1
             np = (p[0] + v[0], p[1] + v[1])
             if np[0] < 0 or np[1] < 0 or np[0] == width or np[1] == height:
                 break  # We fell off the map
-            if map[np[1]][np[0]] == "#":
+            if map[np[1]][np[0]] == 35:
                 # We hit a wall and turned
                 v = DIRECTION_VECTOR_CHANGES[v]
             else:
@@ -81,7 +81,7 @@ def stuck(map, p, width, height):
         np = (p[0] + v[0], p[1] + v[1])
         if np[0] < 0 or np[1] < 0 or np[0] == width or np[1] == height:
             break  # We fell off the map
-        if map[np[1]][np[0]] == "#":
+        if map[np[1]][np[0]] == 35:
             # We hit a wall and turned
             v = DIRECTION_VECTOR_CHANGES[v]
         else:
@@ -94,20 +94,20 @@ def stuck(map, p, width, height):
     return False
 
 
-@profile
+# @profile
 def part2(positions):
     map = load_input("day6.txt")
     width = len(map[0])
     height = len(map)
-    start = find(map, "^")
+    start = find(map, 94)
 
     total = 0
     for p in positions:
-        if map[p[1]][p[0]] == ".":
-            map[p[1]][p[0]] = "#"
+        if map[p[1]][p[0]] == 46:
+            map[p[1]][p[0]] = 35
             if stuck(map, start, width, height):
                 total += 1
-            map[p[1]][p[0]] = "."
+            map[p[1]][p[0]] = 46
     return total
 
 
