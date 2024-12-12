d90e901d56a4f20d9007c0fd63107a3042631cc3
Stefan Arentz <stefan@arentz.ca>
2024-12-11 21:28:29 -0500

    2024 Python Day 11

--- 2024/py/day11.py
+++ 2024/py/day11.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 from functools import cache
-from itertools import batched
 from math import floor, log10
 
 INPUT = (92, 0, 286041, 8034, 34394, 795, 8, 2051489)
@@ -10,20 +9,18 @@ ROUNDS1 = 25
 ROUNDS2 = 75
 
 
-def transform_list(numbers: list[int]) -> list[int]:
-    result = []
-    for n in numbers:
-        if n == 0:
-            result.append(1)
+@cache
+def apply_rules(n: int) -> list[int]:
+    if n == 0:
+        return [1]
+    else:
+        digits = floor(log10(n)) + 1
+        if digits % 2 == 0:
+            div = 10 ** (digits // 2)
+            first, second = divmod(n, div)
+            return [first, second]
         else:
-            digits = floor(log10(n)) + 1
-            if digits % 2 == 0:
-                div = 10 ** (digits // 2)
-                first, second = divmod(n, div)
-                result += [first, second]
-            else:
-                result.append(n * 2024)
-    return result
+            return [n * 2024]
 
 
 @cache
@@ -31,12 +28,9 @@ def blink(numbers: tuple[int], max_depth: int, current_depth: int) -> int:
     if current_depth >= max_depth:
         return len(numbers)
 
-    # Why do we get the best results with batch size 1?! Does that
-    # mean all the slowness is really in transform_list?
-
     total = 0
-    for batch in batched(numbers, 1):
-        total += blink(tuple(transform_list(list(batch))), max_depth, current_depth + 1)
+    for number in numbers:
+        total += blink(tuple(apply_rules(number)), max_depth, current_depth + 1)
 
     return total
 
