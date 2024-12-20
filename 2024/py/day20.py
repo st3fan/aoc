114d541fdf0ecf5bec7b711545185d4c2b87aa53
Stefan Arentz <stefan@arentz.ca>
2024-12-20 16:38:47 -0500

    2024 Python Day 20 Part 1

--- 2024/py/day20.py
+++ 2024/py/day20.py
@@ -18,21 +18,41 @@ def shortest_path(grid: Grid, start: Position, end: Position) -> int:
                 for neighbour in grid.neighbours(p):
                     if grid.get(neighbour) in (".", "S", "E"):
                         g.add_edge((p.x, p.y), (neighbour.x, neighbour.y))
-    return len(nx.shortest_path(g, source=(start.x, start.y), target=(end.x, end.y))) - 1
+    return
 
 
 def part1(grid: Grid) -> int:
     steps = Counter()
+
+    g = nx.Graph()
+    for x in range(grid.width):
+        for y in range(grid.height):
+            p = Position(x, y)
+            if grid.get(p) in (".", "S", "E"):
+                for neighbour in grid.neighbours(p):
+                    if grid.get(neighbour) in (".", "S", "E"):
+                        g.add_edge((p.x, p.y), (neighbour.x, neighbour.y))
+
     if start := grid.find("S"):
         if end := grid.find("E"):
             for x in range(1, grid.width - 1):
                 for y in range(1, grid.height - 1):
                     p = Position(x, y)
+
                     if grid.get(p) == "#":
-                        grid.set(p, ".")
-                        c = shortest_path(grid, start, end)
+                        neighbours = grid.neighbours(p)
+                        for n in neighbours:
+                            if grid.get(n) in (".", "S", "E"):
+                                g.add_edge((p.x, p.y), (n.x, n.y))
+
+                        c = len(nx.shortest_path(g, source=(start.x, start.y), target=(end.x, end.y))) - 1
                         steps.update([c])
-                        grid.set(p, "#")
+
+                        for n in neighbours:
+                            try:
+                                g.remove_edge((p.x, p.y), (n.x, n.y))
+                            except:
+                                pass
 
                         # for n in grid.neighbours(p):
                         #     o = grid.get(n)
@@ -52,9 +72,16 @@ def part1(grid: Grid) -> int:
 
     pprint(savings, indent=3)
 
-    return 0
+    t = 0
+    for s, n in savings.items():
+        if s >= 100:
+            t += n
+
+    print("TOTAL", t)
+
+    return t
 
 
 if __name__ == "__main__":
-    grid = Grid.from_file("day20_test.txt", lambda v: v)
+    grid = Grid.from_file("day20.txt", lambda v: v)
     print("Part1:", part1(grid))
