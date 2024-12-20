0949b2e44fa24c8edf136f6c725bf04b8a9da1e0
Stefan Arentz <stefan@arentz.ca>
2024-12-20 18:15:52 -0500

    2024 Python Day 20 Part 1

--- 2024/py/day20.py
+++ 2024/py/day20.py
@@ -9,18 +9,6 @@ import networkx as nx
 from aoc import Grid, Position
 
 
-def shortest_path(grid: Grid, start: Position, end: Position) -> int:
-    g = nx.Graph()
-    for x in range(grid.width):
-        for y in range(grid.height):
-            p = Position(x, y)
-            if grid.get(p) in (".", "S", "E"):
-                for neighbour in grid.neighbours(p):
-                    if grid.get(neighbour) in (".", "S", "E"):
-                        g.add_edge((p.x, p.y), (neighbour.x, neighbour.y))
-    return
-
-
 def part1(grid: Grid) -> int:
     steps = Counter()
 
@@ -35,34 +23,24 @@ def part1(grid: Grid) -> int:
 
     if start := grid.find("S"):
         if end := grid.find("E"):
+            source = (start.x, start.y)
+            target = (end.x, end.y)
             for x in range(1, grid.width - 1):
                 for y in range(1, grid.height - 1):
                     p = Position(x, y)
 
                     if grid.get(p) == "#":
-                        neighbours = grid.neighbours(p)
-                        for n in neighbours:
+                        new_edges = []
+                        for n in grid.neighbours(p):
                             if grid.get(n) in (".", "S", "E"):
-                                g.add_edge((p.x, p.y), (n.x, n.y))
+                                e = ((p.x, p.y), (n.x, n.y))
+                                new_edges.append(e)
+                                g.add_edge(e[0], e[1])
 
-                        c = len(nx.shortest_path(g, source=(start.x, start.y), target=(end.x, end.y))) - 1
-                        steps.update([c])
+                        steps.update([len(nx.shortest_path(g, source=source, target=target)) - 1])
 
-                        for n in neighbours:
-                            try:
-                                g.remove_edge((p.x, p.y), (n.x, n.y))
-                            except:
-                                pass
-
-                        # for n in grid.neighbours(p):
-                        #     o = grid.get(n)
-                        #     if o in (".", "E"):
-                        #         grid.set(n, ".")
-                        #         c = shortest_path(grid, start, end)
-                        #         steps.update([c])
-                        #         grid.set(n, o)
-
-    # pprint(steps)
+                        for e in new_edges:
+                            g.remove_edge(e[0], e[1])
 
     savings = {}
     for s, n in steps.items():
