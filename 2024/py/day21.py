a2080b7f59f949ecca12c0287a3db74f5cb769e5
Stefan Arentz <stefan@arentz.ca>
2024-12-26 16:43:56 -0500

    2024 Python Day 21

---
+++ 2024/py/day21.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+
+from itertools import pairwise
+
+import networkx as nx
+
+# +---+---+---+
+# | 7 | 8 | 9 |
+# +---+---+---+
+# | 4 | 5 | 6 |
+# +---+---+---+
+# | 1 | 2 | 3 |
+# +---+---+---+
+#     | 0 | A |
+#     +---+---+
+
+
+def build_numeric_keypad_graph() -> nx.DiGraph:
+    g = nx.DiGraph()
+    movements = {
+        "A": [("0", "<"), ("3", "^")],
+        "0": [("A", ">"), ("2", "^")],
+        "1": [("2", ">"), ("4", "^")],
+        "2": [("0", "v"), ("1", "<"), ("3", ">"), ("5", "^")],
+        "3": [("A", "v"), ("2", "<"), ("6", "^")],
+        "4": [("1", "v"), ("5", ">"), ("7", "^")],
+        "5": [("2", "v"), ("4", "<"), ("6", ">"), ("8", "^")],
+        "6": [("3", "v"), ("5", "<"), ("9", "^")],
+        "7": [("4", "v"), ("8", ">")],
+        "8": [("5", "v"), ("7", "<"), ("9", ">")],
+        "9": [("6", "v"), ("8", "<")],
+    }
+    for src, dst in movements.items():
+        for d in dst:
+            g.add_edge(src, d[0], move=d[1])
+    return g
+
+
+def num_movements(g: nx.DiGraph, src: str, dst: str) -> list[str]:
+    moves = []
+    for a, b in pairwise(nx.shortest_path(g, source=src, target=dst)):
+        moves.append(g.get_edge_data(a, b)["move"])
+    return moves
+
+
+#     +---+---+
+#     | ^ | A |
+# +---+---+---+
+# | < | v | > |
+# +---+---+---+
+
+
+def build_directional_keypad_graph() -> nx.DiGraph:
+    g = nx.DiGraph()
+    movements = {
+        "A": [("^", "<"), (">", "v")],
+        "<": [("v", ">")],
+        ">": [("v", "<"), ("A", "^")],
+        "^": [("A", ">"), ("v", "v")],
+        "v": [("<", "v"), (">", ">"), ("^", "^")],
+    }
+    for src, dst in movements.items():
+        for d in dst:
+            g.add_edge(src, d[0], move=d[1])
+    return g
+
+
+def dir_movements(g: nx.DiGraph, src: str, dst: str) -> list[str]:
+    moves = []
+    for a, b in pairwise(nx.shortest_path(g, source=src, target=dst)):
+        moves.append(g.get_edge_data(a, b)["move"])
+    return moves
+
+
+# def dir_movements(key: str) -> list[str]:
+#     match key:
+#         case "A":
+#             return ["A"]
+#         case "<":
+#             return ["v", "<", "<", "A", ">", ">", "^"]
+#         case ">":
+#             return ["v", "A", "^"]
+#         case "^":
+#             return ["<", "A", ">"]
+#         case "v":
+#             return ["v", "<", "A", ">", "^"]
+#     raise ValueError(f"Invalid key: {key}")
+
+
+# <vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
+# v<<A>>^A<A>AvA<^AA>A<vAAA>^A
+# <A^A>^^AvvvA
+# 029A
+
+
+def part1(codes: list[str]) -> int:
+    num_move_graph = build_numeric_keypad_graph()
+    dir_move_graph = build_directional_keypad_graph()
+
+    # print(movements(g, "A", "7"))
+
+    test_codes = [
+        "029A",
+        "980A",
+        "179A",
+        "456A",
+        "379A",
+    ]
+
+    t = 0
+
+    for code in test_codes:
+        #
+        # First directional keypad
+        #
+
+        moves1 = []
+        for a, b in pairwise("A" + code):
+            moves1 += num_movements(num_move_graph, a, b)
+            moves1.append("A")
+        # print("".join(moves1))
+
+        moves2 = []
+        for a, b in pairwise(["A"] + moves1):
+            moves2 += dir_movements(dir_move_graph, a, b)
+            # moves2 += dir_movements(dir_move_graph, b, "A")
+            moves2.append("A")
+        # print("".join(moves2))
+
+        moves3 = []
+        for a, b in pairwise(["A"] + moves2):
+            moves3 += dir_movements(dir_move_graph, a, b)
+            # moves2 += dir_movements(dir_move_graph, b, "A")
+            moves3.append("A")
+
+        print(code + ": " + "".join(moves3) + f" ({len(moves3)})")
+
+        # print("LEN", len(moves3))
+
+        t += len(moves3) * int(code[:-1])
+
+    return t
+
+
+if __name__ == "__main__":
+    print("Part1", part1(["029A"]))
